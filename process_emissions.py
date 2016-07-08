277abdfd963bd5b1648d9972d14d6c4519042c42
Joe Yerardi <joe.yerardi@gmail.com>
2016-07-08 13:06:24 -0700

    Updated process_emissions.py
    I've got it to the point where the company regex iterates through the
    file, pulls out the company names and prints them to a single line of
    the outfile.

--- process_emissions.py
+++ process_emissions.py
@@ -1,21 +1,20 @@
 #! /usr/bin/python3
-​
+
 import re
-​
-# The line
-line = '''SOURCE EMISSIONS
-2013 EMISSIONS INVENTORY REPORT
-CANYON ROCK - MISSION GORGE       Source ID: 27
-7500 MISSION GORGE RD             Contact: BROUWER'''
-​
-# Compile a regex to find the line containing the company name
-company_regex = re.compile(r'(^.+)\s+source id: ', re.IGNORECASE | re.MULTILINE)
-​
-# Run the regex on the line
-company_match = company_regex.match(line)
-​
-# Return the company name
-company = company_match.group(1)
-​
-# Print the company name
-print(company)
+
+# Compile a regex to match the company name
+company_regex = re.compile(r'(^.+)\s+source id:', re.IGNORECASE)
+
+# Pass a blank string to populate the company variable before we process the in-file
+company = ''
+with open('APCD_Emissions_Inventory_Report_2013.txt', 'r') as infile:
+    with open('outfile.csv', 'w') as outfile:
+        outfile.write('"COMPANY"\n')
+        # Iterate through the in-file line-by-line
+        for line in infile:
+            # First, determine whether the current line has a company name
+            if company_regex.match(line):
+                company_match = company_regex.match(line)
+                company = company_match.group(1)
+                # Write all matched companies to a single line
+                outfile.write(company)
