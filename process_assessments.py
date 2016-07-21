a6551843ff5c2e72eef846650c159c12f1447bdb
Joe Yerardi <joe.yerardi@gmail.com>
2016-07-21 16:06:35 -0700

    Added/modified print commands
    Modified the first print command to print the entire line_out_1 variable
    instead of just the bits variable
    Added a second print command to print the entire line_out_2 variable

--- process_assessments.py
+++ process_assessments.py
@@ -18,20 +18,24 @@ with open('Data/Assessments Data/assessments_1516.txt', 'r') as infile:
                 fund_num = fund_num_match.group(1)
             # If that fails, determine if the current line has assessment data
             elif assessment_data_regex.match(line):
-                # Split the matched lines into columns based on white space
+                # Split the matched line into columns based on white space
                 columns = line.split()
                 # Pass all columns other than odd cent (those equal to .01)
                 # to the variable bits
                 bits = [column for column in columns if column != '.01']
-                # Print the line to the terminal
-                print("bits = {0}".format(bits))
-                # Populate a line_out variable with the first (or only) set of assessment
-                # data and write it to the outfile
+                # Pass the fund number, parcel number and net fee from the first
+                # (or only) set of assessment data to the variable line_out_1
                 line_out_1 = '"{0}","{1}","{2}"\n'.format(fund_num, bits[0], bits[2])
+                # Print the line_out_1 variable to the terminal
+                print(line_out_1)
+                # Write the line_out_1 variable to the outfile
                 outfile.write(line_out_1)
                 # Determine if the current line has a second set of assessment data
                 if len(bits) > 3:
-                    # Populate a second line_out variable with the second set of
-                    # assessment data and write it to the outfile
+                    # Pass the fund number, parcel number and net fee from the
+                    # second set of assessment data to the variable line_out_2
                     line_out_2 = '"{0}","{1}","{2}"\n'.format(fund_num, bits[3], bits[5])
+                    # Print the line_out_2 variable to the terminal
+                    print(line_out_2)
+                    # Write the line_out_2 variable to the terminal
                     outfile.write(line_out_2)
